9c7d5d99411d944af562fb271be43dd3124893f9
mfcastilho <fredericocastilho@hotmail.com>
2024-06-07 16:16:35 -0300

    feat: insert a useEffect at the dafaul.layout and inside this to controler the page width, if its bigger than 639px the menu buger close automatically

--- src/layouts/default.layout.tsx
+++ src/layouts/default.layout.tsx
@@ -1,14 +1,25 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import './default.layout.scss';
 import { Header } from '../components/header/Header';
 import { Footer } from '../components/footer/Footer';
 import { useAppContext } from '../context/AppContext';
-import { Sidebar } from '../components/sidebar-refactor/SidebarRefactor'
-// import { Navbar } from '../components/Navbar'; COMPONENT SUGGESTION
+import { Sidebar } from '../components/sidebar/Sidebar';
 
 export const DefaultLayout = () => {
-  const { selectedMenu } = useAppContext();
-  
+  const { selectedMenu, setSelectedMenu } = useAppContext();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 639 && selectedMenu) {
+        setSelectedMenu(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [selectedMenu, setSelectedMenu]);
+
   return (
     <div className="app">
       <Header />
