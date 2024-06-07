043c2be6476ca94fd2e779dbbc6ce5686c8e9e5c
Esther Guimarães <estherguimaraesl@gmail.com>
2024-06-07 13:20:40 -0300

    feat: implemented pagination layout

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,120 +1,25 @@
-import classNames from 'classnames';
+import { PaginationProps } from '../../types/PaginationProps';
+import arrowLeft from '/img/icons/arrow-left-color-white.svg';
+import arrowRight from '/img/icons/arrow-right-color-white.svg';
 import './Pagination.scss';
-import React, { useEffect, useRef, useState } from 'react';
 
-type Props = {
-  pages: number;
-  currentPage: string;
-  setPage: (v: string) => void;
-};
-
-export const Pagination: React.FC<Props> = ({ pages, currentPage, setPage }) => {
-  const firstBlock = 0;
-  const lastBlock = pages / 4 - 1;
-
-  const pagesArray = Array.from({ length: pages }, (_, index) => (1 + index).toString());
-
-  const [currentBlock, setCurrentBlock] = useState(firstBlock);
-
-  const isFirstBlock = currentBlock === firstBlock;
-  const isLastBlock = currentBlock === lastBlock;
-
-  const blockFirstPage = currentBlock * 4 + 2;
-  const blockLastPage = (currentBlock + 1) * 4;
-
-  const isBlockFirstPage = +currentPage === blockFirstPage;
-  const isBlockLastPage = +currentPage === blockLastPage;
-
-  const list = useRef<HTMLUListElement>(null);
-
-  useEffect(() => {
-    if (currentPage === '1') {
-      setCurrentBlock(firstBlock);
-    }
-
-    if (currentPage === pages.toString()) {
-      setCurrentBlock(lastBlock);
-    }
-  }, [currentPage, lastBlock, pages]);
-
-  const onLeftMove = () => {
-    if (currentPage !== '1') {
-      setPage((+currentPage - 1).toString());
-      if (+currentPage <= blockFirstPage) {
-        setCurrentBlock(currentBlock - 1);
-      }
-    }
-  };
-
-  const onRightMove = () => {
-    if (!(isBlockLastPage && isLastBlock)) {
-      setPage((+currentPage + 1).toString());
-      if (+currentPage >= blockLastPage) {
-        setCurrentBlock(currentBlock + 1);
-      }
-    }
-  };
-
+export const Pagination = () => {
   return (
     <div className="pagination">
-      <button className="pagination__left-slide" onClick={onLeftMove}>
-        <div
-          className={
-            isFirstBlock && isBlockFirstPage ? 'icon icon-left-disabled' : 'icon icon-left'
-          }
-        />
-      </button>
-
-      <div className="pagination__slider">
-        <li className="pagination__slider-item" key={1}>
-          <button
-            className={classNames('pagination__slider-num', {
-              'pagination__slider-num-active': currentPage === '1',
-            })}
-            onClick={() => setPage('1')}
-          >
-            {1}
-          </button>
-        </li>
-        {currentBlock !== 0 && <p className="pagination__slider-dots">...</p>}
-        <ul className="pagination__slider-list" ref={list}>
-          {pagesArray.slice(blockFirstPage - 1, blockLastPage + 1).map(pageNum => (
-            <li className="pagination__slider-item" key={pageNum}>
-              <button
-                className={classNames('pagination__slider-num', {
-                  'pagination__slider-num-active': currentPage === pageNum,
-                })}
-                onClick={() => setPage(pageNum)}
-              >
-                {pageNum}
-              </button>
-            </li>
-          ))}
-        </ul>
-        {currentBlock !== lastBlock && (
-          <>
-            <p className="pagination__slider-dots">...</p>
-            <li className="pagination__slider-item" key={pages}>
-              <button
-                className={classNames('pagination__slider-num', {
-                  'pagination__slider-num-active': currentPage === pages.toString(),
-                })}
-                onClick={() => setPage(pages.toString())}
-              >
-                {pages}
-              </button>
-            </li>
-          </>
-        )}
+      <div className="pagination__button pagination__button--disabled">
+        <img className="pagination__button__previous" src={arrowLeft} alt="previous-button" />
       </div>
 
-      <button className="pagination__right-slide" onClick={onRightMove}>
-        <div
-          className={
-            isLastBlock && isBlockLastPage ? 'icon icon-right-disabled' : 'icon icon-right'
-          }
-        />
-      </button>
+      <ul className="numbers pagination__numbers">
+        <li className="numbers__page numbers__page--active">1</li>
+        <li className="numbers__page ">2</li>
+        <li className="numbers__page">3</li>
+        <li className="numbers__page">4</li>
+      </ul>
+
+      <div className="pagination__button">
+        <img className="pagination__button__next" src={arrowRight} alt="next-button" />
+      </div>
     </div>
   );
 };
