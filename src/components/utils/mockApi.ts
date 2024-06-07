46c71159bf4459286cd6902d4c08eea638552363
mfcastilho <fredericocastilho@hotmail.com>
2024-06-07 03:04:14 -0300

    fix: fix some imports errors

--- src/components/utils/mockApi.ts
+++ src/components/utils/mockApi.ts
@@ -1,6 +1,6 @@
 // src/api/mockApi.ts
-import products from '../../public/api/products.json';
-import { ProductType } from '../types/ProductType';
+import products from '../../../public/api/products.json';
+import { ProductType } from '../../types/ProductType';
 
 export const fetchProducts = async (): Promise<ProductType[]> => {
   // Simulando uma requisição assíncrona
