675d7ef82ca1a00c8ba60a6d6ac9e2903b776cb7
HigorPedroso <higor533@gmail.com>
2024-06-08 18:08:28 -0300

    improves phone grid

--- src/utils/mockApi.ts
+++ src/utils/mockApi.ts
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-// src/api/mockApi.ts
-=======
->>>>>>> 8d1a53a4eb85cf83d5e71cfe8bb252a16a1e5ee1
 import products from '../../public/api/products.json';
 import { ProductType } from '../types/ProductType';
 
 export const fetchProducts = async (): Promise<ProductType[]> => {
-<<<<<<< HEAD
-  // Simulando uma requisição assíncrona
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(products as ProductType[]);
-    }, 500); // Simulando um delay de 500ms
-=======
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(products as ProductType[]);
     }, 500);
->>>>>>> 8d1a53a4eb85cf83d5e71cfe8bb252a16a1e5ee1
   });
 };
