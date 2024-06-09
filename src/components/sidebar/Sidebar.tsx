aabff184ecdcea098b570e25a6b120d607ea43b6
mfcastilho <fredericocastilho@hotmail.com>
2024-06-09 04:29:12 -0300

    feat: finalize Sidebar component structure

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,8 +4,7 @@ import { useAppContext } from '../../context/AppContext';
 import { useEffect, useState } from 'react';
 
 export const Sidebar: React.FC = () => {
-  const { favorites, cart, selectedNavItem, setSelectedNavItem, selectedMenu } = useAppContext();
-  const [selected, setSelected] = useState('Home');
+  const { favorites, cart, selectedNavItem, setSelectedNavItem, selectedMenu, setSelectedMenu } = useAppContext();
   const [cartCounter, setCartCounter] = useState(3);
   const [favoriteCounter, setFavoriteCounter] = useState(5);
   const location = useLocation();
@@ -17,11 +16,9 @@ export const Sidebar: React.FC = () => {
 
   useEffect(() => {
     const pathname = location.pathname;
-    const page =
-      pathname === '/' ? 'Home' : pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2);
-    setSelected(page);
-    setSelectedNavItem(selected);
-  }, [location]);
+    const page = pathname === '/' ? 'Home' : pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2);
+    setSelectedNavItem(page);
+  }, [location, setSelectedNavItem]);
 
   useEffect(() => {
     if (selectedMenu) {
@@ -31,26 +28,86 @@ export const Sidebar: React.FC = () => {
     }
   }, [selectedMenu]);
 
+  const handleMenuItemClick = (page: string) => {
+    setSelectedNavItem(page);
+    setSelectedMenu(false);
+  };
+
   return (
-    <aside className="page__menu menu" id="menu">
+    <aside className="menu" id="menu">
       <div className="container">
-        <div className="menu__bottom">
+        <div className="menu__content">
           <nav className="menu-nav menu__nav">
             <ul className="menu-nav__list">
               <li className="menu-nav_item">
-                <Link className={`menu-nav__link menu-nav__link ${selectedNavItem === 'Home' ? 'is-active' : ''}`} to="/">Home</Link>
+                <Link
+                  className={`menu-nav__link ${selectedNavItem === 'Home' ? 'is-active' : ''}`}
+                  to="/"
+                  onClick={() => handleMenuItemClick('Home')}
+                >
+                  Home
+                </Link>
               </li>
               <li className="menu-nav_item">
-                <Link className={`menu-nav__link menu-nav__link ${selectedNavItem === 'Phones' ? 'is-active' : ''}`} to="/phones">Phones</Link>
+                <Link
+                  className={`menu-nav__link ${selectedNavItem === 'Phones' ? 'is-active' : ''}`}
+                  to="/phones"
+                  onClick={() => handleMenuItemClick('Phones')}
+                >
+                  Phones
+                </Link>
               </li>
               <li className="menu-nav_item">
-                <Link className={`menu-nav__link menu-nav__link ${selectedNavItem === 'Tablets' ? 'is-active' : ''}`} to="/tablets">Tablets</Link>
+                <Link
+                  className={`menu-nav__link ${selectedNavItem === 'Tablets' ? 'is-active' : ''}`}
+                  to="/tablets"
+                  onClick={() => handleMenuItemClick('Tablets')}
+                >
+                  Tablets
+                </Link>
               </li>
               <li className="menu-nav_item">
-                <Link className={`menu-nav__link menu-nav__link ${selectedNavItem === 'Accessories' ? 'is-active' : ''}`} to="/accessories">Accessories</Link>
+                <Link
+                  className={`menu-nav__link ${selectedNavItem === 'Accessories' ? 'is-active' : ''}`}
+                  to="/accessories"
+                  onClick={() => handleMenuItemClick('Accessories')}
+                >
+                  Accessories
+                </Link>
               </li>
             </ul>
           </nav>
+
+          <div className="menu__bottom-part bottom-part">
+            <div className="bottom-part__item-box">
+              <Link
+                to="/favorite"
+                className={`bottom-part__icon ${selectedNavItem === 'Favorite' ? 'is-active' : ''}`}
+                onClick={() => handleMenuItemClick('Favorite')}
+              >
+                <div className="bottom-part__icon--favorite icon-wrapper">
+                {favoriteCounter ? (
+                  <div className="bottom-part__icon--counter">{favoriteCounter}</div>
+                ) : (
+                  ''
+                )}
+                </div> 
+              </Link>
+            </div>
+
+            <div className="bottom-part__item-box">
+              <Link
+                to="/cart"
+                className={`bottom-part__icon ${selectedNavItem === 'Cart' ? 'is-active' : ''}`}
+                onClick={() => handleMenuItemClick('Cart')}
+              >
+                <div className="bottom-part__icon--cart icon-wrapper">
+                  {cartCounter ? <div className="bottom-part__icon--counter">{cartCounter}</div> : ''}
+                </div>
+              </Link>
+            </div>
+
+          </div>
         </div>
       </div>
     </aside>
