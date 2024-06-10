a106f1f3e92893f99be94014d6b0bfa37e18b4f4
HigorPedroso <higor533@gmail.com>
2024-06-10 16:40:12 -0300

    fix conflits

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,10 +1,7 @@
-import { ProductsSlider } from "../../components/products-slider/ProductsSlider";
-
 export const HomePage = () => {
   return ( 
     <>
       <h1 className="title">Phone Catalog</h1>;
-      <ProductsSlider />
     </>
   )
   
