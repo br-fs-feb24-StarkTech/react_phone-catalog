98eb9475bff018ae025cc84ad7fbb21122eea2e8
eduvinagre <eduvinagre@gmail.com>
2024-06-12 10:44:12 -0300

    feat: creat route and pages for tablets and accessories

---
+++ src/pages/accessories-page/AccessoriesPage.jsx
@@ -0,0 +1,10 @@
+import './AccessoriesPage.scss';
+
+export const AccessoriesPage = () => {
+  return (
+    <div>
+      <h1>Accessories Page</h1>
+      <p>List of accessories will be displayed here.</p>
+    </div>
+  );
+};
---
+++ src/pages/tablets-page/TabletsPage.jsx
@@ -0,0 +1,10 @@
+import './TabletsPage.scss';
+
+export const TabletsPage = () => {
+  return (
+    <div>
+      <h1>Tablets Page</h1>
+      <p>List of tablets will be displayed here.</p>
+    </div>
+  );
+};
