b1100b7465958be5594c30409e35ba54dc265b94
HigorPedroso <higor533@gmail.com>
2024-06-13 20:24:25 -0300

    new models

---
+++ src/components/new-models/NewModels.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+import { ProductsSlider } from "../product-slider/ProductSlider"
+import { getNewProducts } from "../../services/Products";
+import { ProductType } from "../../types/ProductType";
+
+export const NewModels = () => {
+
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    getNewProducts()
+      .then(product => setProducts(product));
+  }, []);
+
+  return (
+    <ProductsSlider products={products} title="Brand new models" />
+  )
+}
