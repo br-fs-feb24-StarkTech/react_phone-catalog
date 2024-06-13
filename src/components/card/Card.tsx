1a7d95ad91a8aec492a3b70c1ac4773488340e37
HigorPedroso <higor533@gmail.com>
2024-06-13 08:50:03 -0300

    add link card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,37 +1,48 @@
 import './Card.scss';
 import { ActionButtons } from '../action-buttons/ActionButtons';
 import { CardProps } from '../../types/CardProps';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Card: React.FC<CardProps> = ({ product }) => {
+
+  const pathname = window.location.pathname;
+  let returnPath = "";
+
+  if(pathname !== "/") {
+    returnPath = "../";
+  }
+
   return (
-      <li className="products__card card">
+    <li className="products__card card">
+      <Link to={`${returnPath}item-details/${product.itemId}`} style={{ textDecoration: 'none' }}>
         <img src={product.image} alt="img-phone" className="card__img" />
 
         <h4 className="card__title">{product.name}</h4>
+      </Link>
 
-        <h3 className="card__price">
-          $ {product.price} <span className="card__price--offer">$ {product.fullPrice}</span>
-        </h3>
+      <h3 className="card__price">
+        $ {product.price} <span className="card__price--offer">$ {product.fullPrice}</span>
+      </h3>
 
-        <div className="card__line"></div>
+      <div className="card__line"></div>
 
-        <div className="card__description description">
-          <p className="description__text">
-            <span>Screen</span>
-            <span className="description__text--modify">{product.screen}</span>
-          </p>
-          <p className="description__text">
-            <span>Capacity</span>
-            <span className="description__text--modify">{product.capacity}</span>
-          </p>
-          <p className="description__text">
-            <span>RAM</span>
-            <span className="description__text--modify">{product.ram}</span>
-          </p>
-        </div>
+      <div className="card__description description">
+        <p className="description__text">
+          <span>Screen</span>
+          <span className="description__text--modify">{product.screen}</span>
+        </p>
+        <p className="description__text">
+          <span>Capacity</span>
+          <span className="description__text--modify">{product.capacity}</span>
+        </p>
+        <p className="description__text">
+          <span>RAM</span>
+          <span className="description__text--modify">{product.ram}</span>
+        </p>
+      </div>
 
-        <ActionButtons product={product} />
-      </li>
+      <ActionButtons product={product} />
+    </li>
   );
 };
 
