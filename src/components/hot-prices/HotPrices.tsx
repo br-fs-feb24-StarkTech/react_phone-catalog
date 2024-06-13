9fdb0be56aa307f70cd25d55451ae4dae0eba5d1
HigorPedroso <higor533@gmail.com>
2024-06-13 19:54:35 -0300

    hot prices

---
+++ src/components/hot-prices/HotPrices.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+import { ProductsSlider } from "../product-slider/ProductSlider"
+import { ProductType } from "../../types/ProductType";
+import { getHotPriceProducts } from "../../services/Products";
+
+export const HotPrices = () => {
+
+  const [products, setProduct] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    getHotPriceProducts()
+      .then(product =>setProduct(product));
+    }, []);
+
+  return (
+    <ProductsSlider products={products} title="Hot prices"/>
+  )
+}
