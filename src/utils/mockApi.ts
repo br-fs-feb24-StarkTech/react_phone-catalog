74a0126d742a6a2e3328eacd14e20afaeb3e6a03
eduvinagre <eduvinagre@gmail.com>
2024-06-14 11:41:04 -0300

    fix: merge fix

--- src/utils/mockApi.ts
+++ src/utils/mockApi.ts
@@ -3,14 +3,6 @@ import phones from '../../public/api/phones.json';
 import { ProductType } from '../types/ProductType';
 import { ProductDetails } from '../types/ProductDetails';
 
-export const fetchSliderProducts = async (): Promise<ProductType[]> => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(products.slice(0, 20) as ProductType[]);
-    }, 500);
-  });
-};
-
 export const fetchProducts = async (): Promise<ProductType[]> => {
   return new Promise(resolve => {
     setTimeout(() => {
