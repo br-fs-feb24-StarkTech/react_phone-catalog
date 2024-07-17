4117c5920ee1c239869dfce9dc2f4637cffc0e0c
Esther Guimarães <estherguimaraesl@gmail.com>
2024-07-17 18:19:14 -0300

    feat: integration recommended endpoint

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Category } from '../../components/category/Category';
 import { ProductType } from '../../types/ProductType';
-import { fetchProducts } from '../../utils/mockApi';
+import { fetchProducts } from '../../services/mockApi';
 import { Banner } from '../../components/banner/Banner';
 import './HomePage.scss';
 import { HotPrices } from '../../components/hot-prices/HotPrices';
--- src/services/mockApi.ts
+++ src/services/mockApi.ts
@@ -1,8 +1,9 @@
 import { ProductDetails } from '../types/ProductDetails';
 import { ProductsResponse } from '../types/ProductsResponse';
 import axios from 'axios';
+import { ProductType } from '../types/ProductType';
 
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchProducts = async (category: string, page: number, limit: number) => {
   return await axios.get<ProductsResponse>(`${BASE_URL}/products/`, {
@@ -16,4 +17,8 @@ export const fetchProducts = async (category: string, page: number, limit: numbe
 
 export const fetchProductDetails = async (productId?: string) => {
   return await axios.get<ProductDetails>(`${BASE_URL}/products/${productId}`);
+};
+
+export const fetchRecommendedProducts = async (productId: string) => {
+  return await axios.get<ProductType[]>(`${BASE_URL}/products/${productId}/recommended`);
 };
