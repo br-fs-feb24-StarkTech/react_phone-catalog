93ada4c275205002e93c9e733bfcda1a9a2afec8
mfcastilho <fredericocastilho@hotmail.com>
2024-07-18 15:12:14 -0300

    feat: insert useAuthContext and toastfy in LoginPage

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,10 +1,68 @@
 import React, { useState } from 'react';
+import { useAuthContext } from '../../context/AuthContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.scss';
 
 const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isActive, setIsActive] = useState(false);
+  const { login } = useAuthContext();
+  const navigate = useNavigate();
 
   const toggleActive = () => setIsActive(!isActive);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:7000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        login(data.token, { name: data.user.username });
+        toast.success('Login bem-sucedido!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          onClose: () => navigate('/home') // Redireciona para '/home' quando o Toast fechar
+        });
+      } else {
+        toast.error('Email ou senha inválidos.', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login', error);
+      toast.error('Ocorreu um erro inesperado. Por favor, tente novamente.', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
 
   return (
     <div className={`container ${isActive ? 'active' : ''}`} id="container">
@@ -27,7 +85,7 @@ const LoginPage: React.FC = () => {
         </form>
       </div>
       <div className="form-container sign-in">
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Sign In</h1>
           <div className="social-icons">
             <a href="#" className="icon">
@@ -38,10 +96,20 @@ const LoginPage: React.FC = () => {
             </a>
           </div>
           <span>or use your email password</span>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
           <a href="#">Forget Your Password?</a>
-          <button>Sign In</button>
+          <button type="submit">Sign In</button>
         </form>
       </div>
       <div className="toggle-container">
@@ -66,6 +134,7 @@ const LoginPage: React.FC = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
